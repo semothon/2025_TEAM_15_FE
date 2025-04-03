85cec1e77b251fa788ec333b005a4f2255747b55
kmin1231 <kmin91143@gmail.com>
2025-04-03 12:45:32 +0900

    🎨 style: #11 import 'Noto Sans' and 'Noto Sans KR' for test

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '@fontsource/alkatra';
+import '@fontsource/noto-sans';
+import '@fontsource/noto-sans-kr';
 
 const Logo = ({ className }) => {
   return (
