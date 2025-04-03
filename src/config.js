f2bdda6c07e6023b8184dfd69331e10f326ed9de
kmin1231 <kmin91143@gmail.com>
2025-04-04 02:17:57 +0900

    ✨ feat: #12 update API endpoints

--- src/config.js
+++ src/config.js
@@ -2,11 +2,27 @@
 
 const CONFIG = {
   // Spring Boot server
+
   SPRING_BOOT: {
-    BASE_URL: 'http://localhost:8090/api/curriculum',
+    BASE_URL: 'http://localhost:8090/api',
     ENDPOINTS: {
-      RECOMMEND: '/recommend',
-      ADD_QUESTION: '/add-ques',
+      LOGIN: '/member/login',
+      SIGNUP: '/member/signup',
+      CHECK_LOGIN_ID: '/member/check-login-id',
+      VERIFY_EMAIL: '/member/verify-email',
+
+      RECOMMEND: '/curriculum/recommend',
+      ADD_QUESTION: '/curriculum/add-ques',
+
+      TIMETABLE: '/api/timetable/get-timetable',
+    },
+  },
+
+  EMAIL_SERVICE: {
+    BASE_URL: 'http://localhost:8090/api/email',
+    ENDPOINTS: {
+      SEND_AUTH_EMAIL: '/send',
+      VERIFY_EMAIL_CODE: '/auth',
     },
   },
 
