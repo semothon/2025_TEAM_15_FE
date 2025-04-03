8ff5d569673ba72fc6f019efbb411d55c97b01ee
kmin1231 <kmin91143@gmail.com>
2025-04-04 02:33:26 +0900

    ✨ feat: #12 implement state management for 'LogIn' page

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,11 +1,49 @@
 // src/pages/LogIn.jsx
 
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Animation from '../components/Animation';
 import '../styles/login.css';
 import '@fontsource/alkatra';
+import CONFIG from '../config';
 
 const LogIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    console.log('이메일:', email);
+    console.log('비밀번호:', password);
+
+    try {
+      const response = await fetch(
+        `${CONFIG.SPRING_BOOT.BASE_URL}${CONFIG.SPRING_BOOT.ENDPOINTS.LOGIN}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        },
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('로그인 성공:', data);
+        navigate('/index');
+      } else {
+        const errorData = await response.json();
+        setErrorMessage(errorData.message || '로그인 실패');
+      }
+    } catch (error) {
+      console.error('에러 발생:', error);
+      setErrorMessage('로그인 중 오류 발생');
+    }
+  };
+
   return (
     <>
       <div className="login-container">
@@ -15,13 +53,24 @@ const LogIn = () => {
           <div className="login-header">
             <h2>로그인</h2>
           </div>
-          <input type="email" placeholder="이메일" className="input-field" />
+          <input
+            type="email"
+            placeholder="이메일"
+            className="input-field"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <input
             type="password"
             placeholder="비밀번호"
             className="input-field"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="login-button">로그인</button>
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+          <button className="login-button" onClick={handleLogin}>
+            로그인
+          </button>
         </div>
       </div>
     </>
