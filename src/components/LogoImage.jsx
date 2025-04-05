3aaad3147bb93ac587046b4146418091418c0844
kmin1231 <kmin91143@gmail.com>
2025-04-06 03:34:05 +0900

    🎨 style: #11 update logo image link to index page

--- src/components/LogoImage.jsx
+++ src/components/LogoImage.jsx
@@ -1,6 +1,8 @@
 // src/components/LogoImage.jsx
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import epiclogo from '../assets/epic-logo.svg';
 import epiclogopng from '../assets/epiclogo.png';
 const LogoImage = () => {
@@ -8,7 +10,9 @@ const LogoImage = () => {
   return (
     <div className="logo-image">
       {/* <img src={epiclogo} alt="Epic Logo" className="logoimage" /> */}
-      <img src={epiclogopng} alt="Epic Logo" className="logoimage" width={105} height={30}/>
+      <Link to="/index">
+        <img src={epiclogopng} alt="Epic Logo" className="logoimage" width={105} height={30} />
+      </Link>
     </div>
   );
 };
