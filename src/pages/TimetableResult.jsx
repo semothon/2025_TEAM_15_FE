114f73233df1a4bd583fdeb818a4f029ffa05b45
kmin1231 <kmin91143@gmail.com>
2025-04-06 02:24:52 +0900

    🐛 fix: #11 update download button

--- src/pages/TimetableResult.jsx
+++ src/pages/TimetableResult.jsx
@@ -89,14 +89,18 @@ const TimetableResult = () => {
           ) : resultImage ? (
             <>
               <img src={resultImage} alt="Result Timetable" className="temp-image" />
-              <button onClick={handleDownloadResultImage} className="download-button">
-                이미지 다운로드
-              </button>
+              
+
             </>
           ) : (
             <p>결과를 불러올 수 없습니다.</p>
           )}
         </div>
+      </div>
+      <div className="timetable-button-container">
+      <button onClick={handleDownloadResultImage} className="download-button">
+                이미지 다운로드 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
+              </button>
       </div>
     </div>
   );
